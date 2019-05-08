c62d2a313dab386f1df8cfb6805f800abbd27ad1
Sam Thomson <samt@samt.st>
2019-05-08 19:55:38 +0000

    more synchronous

--- server/test/server.spec.ts
+++ server/test/server.spec.ts
@@ -9,19 +9,20 @@ chai.use(chaiHttp)
 describe('server', async () => {
 	describe('handle sms', async () => {
 		it('should handle sms as expected', async () => {
+			// post message to api with not enough parameters
+
 			// post message to api with wrong parameters
 
 			// post message with correct parameters but bad values
 
 			// post message with correct paramameters and reasonable values
 
-			return chai
-				.request(server)
-				.post('/handlesms')
-				.then(res => {
-					chai.expect(res.status).to.eql(200)
-					chai.expect(res.text).to.eql('Ok')
-				})
+			const oResponse = await chai.request(server).post('/handlesms')
+
+			// .then(res => {
+			chai.expect(oResponse.status).to.eql(200)
+			chai.expect(oResponse.text).to.eql('Ok')
+			// })
 		})
 	})
 })
