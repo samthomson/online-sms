f643ddc1bad47e6d888021660eda400396523d9f
Sam Thomson <samt@samt.st>
2019-05-08 20:02:47 +0000

    list prospective tests

--- server/test/server.spec.ts
+++ server/test/server.spec.ts
@@ -1,6 +1,6 @@
 import 'mocha'
 import * as chai from 'chai'
-// import { expect, request } from 'chai'
+import { expect, request } from 'chai'
 import chaiHttp = require('chai-http')
 import server from './../src/server'
 
@@ -9,15 +9,21 @@ chai.use(chaiHttp)
 describe('server', async () => {
 	describe('handle sms', async () => {
 		it('should handle sms as expected', async () => {
+			// post message without auth token
+			expect(false).to.eql(true)
+
 			// post message to api with not enough parameters
-			const oResponse = await chai.request(server).post('/handlesms')
+			const oResponse = await request(server).post('/handlesms')
 			chai.expect(oResponse.status).to.eql(422)
 
 			// post message to api with wrong parameters
+			expect(false).to.eql(true)
 
 			// post message with correct parameters but bad values
+			expect(false).to.eql(true)
 
 			// post message with correct paramameters and reasonable values
+			expect(false).to.eql(true)
 		})
 	})
 })
