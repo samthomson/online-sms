4af407f3a76f65df7ffe9b65218223a317a4a7ae
Sam Thomson <samt@samt.st>
2019-05-08 18:40:50 +0000

    testing

---
+++ server/test/server.spec.ts
@@ -0,0 +1,13 @@
+import 'mocha'
+import { expect } from 'chai'
+
+describe('server', async () => {
+	it('should handle sms as expected', async () => {
+		// post message to api with wrong parameters
+
+		// post message with correct parameters but bad values
+
+		// post message with correct paramameters and reasonable values
+		expect(false).to.equal(true)
+	})
+})
