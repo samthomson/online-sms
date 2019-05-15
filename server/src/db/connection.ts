3d415f99525e5f732276a227d2a0adde57a83d2e
Sam Thomson <samt@samt.st>
2019-05-15 10:23:02 +0000

    testing db

--- server/src/db/connection.ts
+++ server/src/db/connection.ts
@@ -1,13 +1,20 @@
 import * as Sequelize from 'sequelize'
 
-const { MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD, MYSQL_DATABASE } = process.env
+const {
+	MYSQL_HOST,
+	MYSQL_USER,
+	MYSQL_PASSWORD,
+	MYSQL_DATABASE,
+	MYSQL_DATABASE_TESTING,
+	NODE_ENV,
+} = process.env
 
 let oConnectionConfig = {
 	host: MYSQL_HOST,
 	port: 3306,
 	username: MYSQL_USER,
 	password: MYSQL_PASSWORD,
-	database: MYSQL_DATABASE,
+	database: NODE_ENV === 'testing' ? MYSQL_DATABASE_TESTING : MYSQL_DATABASE,
 	dialect: 'mysql',
 	dialectOptions: {
 		charset: 'utf8',
--- server/src/db/migrate.ts
+++ server/src/db/migrate.ts
@@ -1,11 +1,13 @@
 import db from './connection'
 import { SMSModel } from './models'
 
-const { MYSQL_DATABASE_TESTING } = process.env
+const { MYSQL_DATABASE, MYSQL_DATABASE_TESTING, NODE_ENV } = process.env
 
 const migrate = async () => {
 	// ensure testing db exists
-	const sRaw: string = `CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE_TESTING}\``
+	const sDBToCreate =
+		NODE_ENV === 'testing' ? MYSQL_DATABASE_TESTING : MYSQL_DATABASE
+	const sRaw: string = `CREATE DATABASE IF NOT EXISTS \`${sDBToCreate}\`;`
 	await db.query(sRaw)
 
 	// create tables
