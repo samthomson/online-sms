edc00a0ac7d531e027f7cc9338942acb0346918e
Sam Thomson <samt@samt.st>
2019-05-15 08:28:54 +0000

    test db

--- server/src/db/migrate.ts
+++ server/src/db/migrate.ts
@@ -1,3 +1,15 @@
+import db from './connection'
 import { SMSModel } from './models'
 
-SMSModel.sync()
+const { MYSQL_DATABASE_TESTING } = process.env
+
+const migrate = async () => {
+	// ensure testing db exists
+	const sRaw: string = `CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE_TESTING}\``
+	await db.query(sRaw)
+
+	// create tables
+	await SMSModel.sync()
+}
+
+migrate()
